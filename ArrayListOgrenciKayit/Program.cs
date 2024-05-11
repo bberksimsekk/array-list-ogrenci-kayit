5e9fc9e4aa7441edb8cb606cd8de38ff37da6b80
bberksimsekk <simsekberk11@gmail.com>
2024-05-11 16:44:46 +0300

    Dosya Atıldı

---
+++ ArrayListOgrenciKayit/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListOgrenciKayit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList ogrenciler = new ArrayList();
+
+            string secenek = "e";
+
+            while (secenek == "e")
+            {
+                Öğrenci ogr = new Öğrenci();
+                Console.WriteLine("Lütfen öğrencinin adını giriniz");
+                ogr.Isim = Console.ReadLine();
+
+                Console.WriteLine("Lütfen öğrencinin soyadını giriniz");
+                ogr.Soyisim = Console.ReadLine();
+
+                Console.WriteLine("Lütfen öğrencinin numarasını giriniz");
+                ogr.OkulNo = Convert.ToInt32(Console.ReadLine());
+
+                ogrenciler.Add(ogr);
+                Console.WriteLine("Öğrenci eklemeye devam edilsin mi ? Evet/Hayır");
+                secenek = Console.ReadLine();
+            }
+
+            Console.WriteLine("*-*-*-*-*Öğrenciler*-*-*-*-*");
+            for (int i = 0; i < ogrenciler.Count; i++)
+            {
+                Öğrenci veri = (Öğrenci)ogrenciler[i];
+                Console.WriteLine(" No = " + veri.OkulNo + ") " + veri.Isim + " " + veri.Soyisim);
+            }
+        }
+    }
+
+    class Öğrenci
+    {
+        public int OkulNo { get; set; }
+
+        public string Isim { get; set; }
+
+        public string Soyisim { get; set; }
+    }
+}
